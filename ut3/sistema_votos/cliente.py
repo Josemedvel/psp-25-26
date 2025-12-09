eff1d8488dbc8f883142f99dfeb2294cc7aa56cb
Josemedvel <josemedvel@gmail.com>
2025-12-09 20:05:51 +0100

    martes 9 diciembre

---
+++ ut3/sistema_votos/cliente.py
@@ -0,0 +1,69 @@
+import socket
+import json
+
+def enviar_mensaje(sock, mensaje):
+    try:
+        sock.send(mensaje.encode())
+    except Exception as e:
+        print("Error al enviar el mensaje:",e)
+
+def recibir_mensaje(sock):
+    try:
+        mensaje = sock.recv(1024)
+        mensaje_json = json.loads(mensaje)
+        return mensaje_json
+    except Exception as e:
+        print("Error al recibir el mensaje:",e)
+
+def gestionar_mensaje_recibido(mensaje):
+    try:
+        resultado = mensaje.get("res")
+        if resultado == "error":
+            error = mensaje.get("error")
+            print("El error que reporta el servidor es: ", error)
+        if resultado == "OK":
+            if "datos" in mensaje: # opciones de voto
+                for i,c in enumerate(mensaje["datos"]):#for i in range(len(mensaje["datos"])):
+                    print(f"{i + 1}.{c}")
+            else:
+                print("Voto realizado exitosamente")
+    except Exception as e:
+        print("Ha habido un error al intentar leer el json:", e)
+
+
+dir_server = ("127.0.0.1", 5000)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.connect(dir_server)
+
+print("Conectados con el server!")
+
+comando = ""
+
+while comando != "FIN":
+    comando = input("Ingresa un comando:\t").strip().upper()
+    match comando:
+        case "CANDIDATOS":
+            json_envio = {
+                "comando": comando
+            }
+            break
+        case "VOTO":
+            candidato = ""
+            while not candidato:
+                candidato = input("Elige un candidato:\t").strip().upper()
+            json_envio = {
+                "comando": comando,
+                "candidato": candidato
+            }
+            break
+        case "FIN":
+            json_envio = {"comando": comando}
+            break
+        case _:
+            print("Por favor, usa un comando válido ('CANDIDATOS', 'VOTO', 'FIN')")
+            continue
+
+    enviar_mensaje(sock, json_envio)
+    mensaje = recibir_mensaje(sock)
+    gestionar_mensaje_recibido(mensaje)
---
+++ ut3/sistema_votos/servidor.py
@@ -0,0 +1,78 @@
+import socket
+import json
+import threading
+import pprint
+
+dir_server = ("127.0.0.1", 5000)
+lista_candidatos = ["A", "B", "C", "D"]
+dicc_votos = {}
+lock = threading.Lock()
+
+def enviar_mensaje(sock, mensaje):
+    try:
+        sock.send(mensaje.encode())
+    except Exception as e:
+        print("Error al enviar el mensaje:",e)
+
+def recibir_mensaje(sock):
+    try:
+        mensaje = sock.recv(1024)
+        mensaje_json = json.loads(mensaje)
+        return mensaje_json
+    except Exception as e:
+        print("Error al recibir el mensaje:",e)
+
+
+def manejar_cliente(sock, direccion_cliente):
+    global dicc_votos
+    fin = False
+    while not fin:
+        mensaje = recibir_mensaje(sock)
+        try:
+            comando = mensaje.get("comando")
+            match comando:
+                case "FIN":
+                    fin = True
+                    continue
+                case "CANDIDATOS":
+                    json_envio = {
+                        "res" : "OK",
+                        "datos" : lista_candidatos
+                    }
+                    break
+                case "VOTAR":
+                    with lock:
+                        if direccion_cliente in dicc_votos: # ya se ha votado
+                            json_envio = {
+                                "res": "error",
+                                "error": "Ya se ha votado anteriormente"
+                            }
+                        else:
+                            opcion = mensaje.get("candidato")
+                            if opcion not in lista_candidatos:
+                                json_envio = {
+                                    "res": "error",
+                                    "error": "Candidato inexistente, comprueba la lista"
+                                }
+                            else:
+                                dicc_votos[direccion_cliente] = opcion
+                                pprint(dicc_votos)
+                    json_envio = {
+                        "res" : "OK"
+                    }
+            enviar_mensaje(json_envio)
+        except Exception as e:
+            print("Error al desempaquetar el json:", e)
+            
+    sock.close()
+
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(dir_server)
+sock.listen()
+
+while True:
+    cliente, dir_cliente = sock.accept()
+    threading.Thread(target=manejar_cliente, args=(cliente, dir_cliente)).start()
